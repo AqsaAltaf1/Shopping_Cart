9c8e0f89afdf31fe786261656f77bfd2c38de403
AqsaAltaf1 <AqsaAltaf1@github.com>
2023-07-11 16:55:12 +0500

    devise added

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+  # GET /resource/sign_in
+  before_action :intialize_cart, only: [:create]
+
+  def intialize_cart
+    if current_user.present?
+      if current_user.cart.nil?
+        current_user.create_cart 
+      end
+    end 
+  end
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  def create
+    super do |resource|
+      unless resource.active?
+        sign_out
+        flash[:alert] = "Yor are not active yet."
+        redirect_to root_path
+        return
+      end
+    end
+  end
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+
+  def after_sign_in_path_for(_resource)
+    if current_user.SuperAdmin?
+     users_index_path
+    else
+      root_path
+    end
+  end 
+
+  #  def after_sign_in_path_for(_resource)
+  #   if current_user&.inactive?
+  #     root_path
+  #   else
+  #     users_path
+  #   end
+  # end 
+  # def after_sign_in_path_for(_resource)
+   
+  # end
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
