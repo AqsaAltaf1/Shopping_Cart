79c014b75ad7a3c438c66cfe113677fa52ad3988
AqsaAltaf1 <AqsaAltaf1@github.com>
2023-07-13 12:49:07 +0500

    user edit functionality

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -50,6 +50,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:First_Name, :Last_Name, :City, :Address, :Country,:Phone_number])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:First_Name, :Last_Name, :City, :Address, :Country,:Phone_number])
   end
 
   
