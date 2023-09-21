9bc4831e9171bd1121892fc877501823f09a9b18
hafeez malik <hafeez.rehman@isoftstudios.com>
2023-09-21 16:27:07 +0500

    modal added

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,11 +6,7 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "popper", to: 'popper.js', preload: true
-pin "bootstrap", to: 'bootstrap.min.js', preload: true
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.esm.js"
 pin "jquery", to: "https://code.jquery.com/jquery-3.6.0.min.js"
 pin "DataTable", to: "https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"
-
-
-
-
-
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.create!(
+User.create!([
+    {
     first_name: "aqsa",
     last_name: "altaf",
     email: "aqsaaltaf01@gmail.com",
@@ -16,4 +17,120 @@ User.create!(
     city: 'lahore',
     country: 'Pakistan',
     status: 'active'
-)
+  },
+  {
+    first_name: "mkds",
+    last_name: "altaf",
+    email: "mkds02@gmail.com",
+    password: "aqsa123",
+    role: 'seller',
+    phone_number: '43009197893',
+    address: 'lahore',
+    city: 'lahore',
+    country: 'Pakistan',
+    status: 'active'
+  },
+  {
+    first_name: "sami",
+    last_name: "altaf",
+    email: "sami03@gmail.com",
+    password: "aqsa123",
+    role: 'seller',
+    phone_number: '430097893',
+    address: 'lahore',
+    city: 'lahore',
+    country: 'Pakistan',
+    status: 'active'
+  },
+  {
+
+    first_name: "sajal",
+    last_name: "ali",
+    email: "sajal04@gmail.com",
+    password: "aqsa123",
+    role: 'seller',
+    phone_number: '43009197893',
+    address: 'lahore',
+    city: 'lahore',
+    country: 'Pakistan',
+    status: 'active'
+    },
+    {
+    first_name: "alishba",
+    last_name: "altaf",
+    email: "alishba05@gmail.com",
+    password: "aqsa123",
+    role: 'seller',
+    phone_number: '43009197893',
+    address: 'lahore',
+    city: 'lahore',
+    country: 'Pakistan',
+    status: 'active'
+    },
+    {
+    first_name: "saria",
+    last_name: "khanum",
+    email: "sariya06@gmail.com",
+    password: "aqsa123",
+    role: 'buyer',
+    phone_number: '4300917893',
+    address: 'lahore',
+    city: 'lahore',
+    country: 'Pakistan',
+    status: 'active'
+    },
+    {
+    first_name: "altaf",
+    last_name: "khan",
+    email: "altaf07@gmail.com",
+    password: "aqsa123",
+    role: 'buyer',
+    phone_number: '430091893',
+    address: 'lahore',
+    city: 'lahore',
+    country: 'Pakistan',
+    status: 'active'
+    },
+    {
+    first_name: "zahid",
+    last_name: "khan",
+    email: "zahid07@gmail.com",
+    password: "aqsa123",
+    role: 'buyer',
+    phone_number: '4300917893',
+    address: 'lahore',
+    city: 'lahore',
+    country: 'Pakistan',
+    status: 'active'
+    },
+    {
+    first_name: "saima",
+    last_name: "khanum",
+    email: "saima08@gmail.com",
+    password: "aqsa123",
+    role: 'buyer',
+    phone_number: '4300917893',
+    address: 'lahore',
+    city: 'lahore',
+    country: 'Pakistan',
+    status: 'active'
+    }
+])
+
+Category.create!([
+    {
+        name: 'fashion'
+    },
+    {
+        name: 'furniture'
+    },
+    {
+        name: 'shoes'
+    },
+    {
+        name: 'kitchen'
+    },
+    {
+        name: 'dress'
+    }
+])
